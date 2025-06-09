15211c4e3aaa0bb4d2e1ed4f4facdfcf70425c07
Pablo147dias <pablodias147@gmail.com>
2025-06-09 20:19:59 -0300

    ajustes 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import App from './App.tsx'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from './theme.ts'
