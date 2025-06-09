f0e7c0236f17e2a2d7b69941e4aac48a886031ed
Pablo147dias <pablodias147@gmail.com>
2025-06-09 19:45:23 -0300

    arrumando efeitos Hero

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -7,11 +7,17 @@ import { AnimatedBackground } from "../../../../components/AnimatedBackground/An
 
 const Hero = () => {
 
-  const StyleHero = styled("div")(({theme}) => ({
+  const StyleHero = styled("div")(({theme}) => ({ 
     backgroundColor: theme.palette.primary.main,
     height: "100vh",
     display: "flex",
     alignItems: "center",
+    [theme.breakpoints.up("xs")]: {// Mobile
+    paddingTop: "100px",
+    },
+    [theme.breakpoints.up("md")]: {//Tela Grande
+      paddingTop: "0px",
+    }
   }))
 
   const StyleImage = styled("img")(({theme}) => ({
@@ -32,7 +38,7 @@ const Hero = () => {
                 <Box position="absolute" width={"150%"} top={-100} right={0}>
                   <AnimatedBackground />
                 </Box>
-                <Box position={"absolute"} textAlign={"center"}>
+                <Box position={"relative"} textAlign={"center"}>
                   <StyleImage src={Avatar} alt="avatar logo" />
                 </Box>
               </Box>
