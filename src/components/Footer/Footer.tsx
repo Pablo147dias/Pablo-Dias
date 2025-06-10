5caeac125edb248b7420cba34c8e121c2c8b4c86
Pablo147dias <pablodias147@gmail.com>
2025-06-10 20:19:40 -0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,18 +10,18 @@ const Footer: React.FC = () => {
             <Box pt={2} pb={2}>
                 <Container maxWidth="sm">
                     <Box display="flex" alignItems="center" justifyContent="space-around" pb={1}>
-                        <IconButton onClick={() => window.open("https://github.com/AdrianaSaty")}>
+                        <IconButton onClick={() => window.open("https://github.com/Pablo147dias")}>
                             <GitHubIcon />
                         </IconButton>
-                        <IconButton onClick={() => window.open("https://www.linkedin.com/in/adriana-saty/")}>
+                        <IconButton onClick={() => window.open("https://www.linkedin.com/in/pablo-dias-617243208/")}>
                             <LinkedInIcon />
                         </IconButton>
-                        <IconButton href="mailto:example@example.com" target="_blank">
+                        <IconButton href="mailto:pablodias147@gmail.com" target="_blank">
                             <EmailIcon />
                         </IconButton>
                     </Box>
                     <Typography textAlign="center">
-                        © 2024 Adriana Saty - All rights reserved
+                        © 2025 Pablo Dias - Todos os direitos reservados.
                     </Typography>
                 </Container>
             </Box>
